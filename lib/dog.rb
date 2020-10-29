829b93e9b3e26982aaae6d9627574196effa31cc
Damian Malysza <damian.malysza@gmail.com>
2020-10-29 01:02:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,7 +5,7 @@ class Dog
 
   def initialize(name)
     @name = name
-    @@all << self
+    save
   end
 
   def self.all
@@ -21,6 +21,6 @@ class Dog
   end
 
   def save
-
+    @@all << self
   end
 end
